af51ad99de38acb7049b1828e9cae95be302042a
Adya <adyka013@gmail.com>
2021-10-04 16:03:18 +0700

    menambakan controllers

---
+++ app/Http/Controllers/DataSiswaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DataSiswaController extends Controller
+{
+    public function datasiswa()
+    {
+        $nama = 'ijal';
+        return $nama;
+    }
+    public function datasiswi()
+    {
+        $nama = 'adya';
+        return $nama;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DataSiswaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -46,3 +48,8 @@ Route::group(['prefix' => 'jurusan'], function () {
     });
 
 });
+
+Route::get('data-siswa', [DataSiswaController::class, 'datasiswa']);
+Route::get('data-siswi', [DataSiswaController::class, 'datasiswi']);
+
+Route::resource('user', UserController::class);
